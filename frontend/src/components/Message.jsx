6797489ed3f7d52ebbee40e0786c92616b0258a5
Chris Banks <chrisbanks389@gmail.com>
2024-01-10 17:38:51 -0500

    added remove from cart functionality

--- frontend/src/components/Message.jsx
+++ frontend/src/components/Message.jsx
@@ -2,7 +2,7 @@ import { Alert } from 'react-bootstrap';
 
 const Message = ({ variant, children }) => {
   return (
-    <Alert variant='{variant}'>
+    <Alert variant={variant}>
         {children}
     </Alert>
   )
